47de911f11de298f201ba305b3573ab790a77f4e
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-22 11:54:30 +0700

    variant button

--- src/components/atoms/button/button.tsx
+++ src/components/atoms/button/button.tsx
@@ -7,9 +7,15 @@ interface tButton
     HTMLButtonElement
   > {
   size?: "s" | "m" | "l" | "xl";
+  variant?: "contained" | "outlined";
 }
 
-export default function Button({ children, size = "m", ...props }: tButton) {
+export default function Button({
+  children,
+  size = "m",
+  variant = "contained",
+  ...props
+}: tButton) {
   const sizes = {
     "py-2 px-6": size == "m",
     "py-2 px-4 text-sm": size == "s",
@@ -17,14 +23,15 @@ export default function Button({ children, size = "m", ...props }: tButton) {
     "py-3 px-8 text-xl": size == "xl",
   };
 
+  const variants = {
+    "bg-blue-500 text-white hover:bg-blue-700 rounded-lg":
+      variant == "contained",
+    "bg-none text-blue-500 hover:text-blue-700 border border-blue-500 hover:border-blue-700 rounded-lg font-semibold":
+      variant == "outlined",
+  };
+
   return (
-    <button
-      {...props}
-      className={cn(
-        "bg-blue-500 hover:bg-blue-600 text-white rounded-lg",
-        sizes
-      )}
-    >
+    <button {...props} className={cn(sizes, variants)}>
       {children}
     </button>
   );
