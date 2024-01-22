427755d2284c4da7ba6dfa9a1794f96123067161
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-22 13:29:38 +0700

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
+import Header from "@/components/organisms/header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi provident
-      ratione dolore voluptatibus ut. Sunt illum quis esse dolore aspernatur,
-      asperiores, soluta temporibus in enim expedita sed consequatur nostrum
-      repellendus?
+      <Header />
     </main>
   );
 }
---
+++ src/components/organisms/header.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from "../atoms/button/button";
+
+export default function Header() {
+  return (
+    <header className="grid grid-cols-2 gap-3 mt-20">
+      <div className=" space-y-8">
+        <h1 className="font-bold text-5xl text-teal-950">
+          Tour your dream destination with tourve
+        </h1>
+        <p className="font-medium text-neutral-400 w-[80%]">
+          Plan and book your perfect trip with expert advice, travel tips,
+          destination information and inspiration from us .
+        </p>
+        <Button variant="outlined">Discover Now</Button>
+      </div>
+      <div className="relative flex gap-4">
+        <picture>
+          <img
+            src="https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="vacation 1"
+            className="rounded-lg"
+          />
+        </picture>
+        <picture>
+          <img
+            src="https://images.unsplash.com/photo-1517999349371-c43520457b23?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="vacation 2"
+            className="rounded-lg mt-20"
+          />
+        </picture>
+        {/* <div className="rounded-md bg-white absolute left-20 bottom-10 shadow-md p-2 text-neutral-500 t">
+          Destinasi Impian, Perjalanan Impian
+        </div> */}
+      </div>
+    </header>
+  );
+}
